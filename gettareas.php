3aeab37bcae9eaa0e4aa9c26b35667e6b3c20346
ztioge <egoitzu@telefonica.net>
2016-09-22 09:40:09 +0000

    Consultas

--- gettareas.php
+++ gettareas.php
@@ -4,8 +4,16 @@
 require_once("conexion.php");
 
 try{
-    $sth = $dbh->prepare("Select * from listas;");
+    
+    //Hacemos un filtro para que cuando nos den el ID saber que es un Int.
+    $idlista = filter_input(INPUT_GET, 'ID_listas', FILTER_VALIDATE_INT);
+    
+    //Preparamos la consulta SQL para que se pueda ejecutar.
+    $sth = $dbh->prepare("Select * from tareas where ID_listas = $idlista ;");
+    
+    //Ejecutamos la consulta
     $sth->execute();
+    
     
     $result = $sth->fetchAll(PDO::FETCH_ASSOC);
     
